8fa94ee7847e5571bbcf7130b080fc4e65635c95
shubhamqx <shubhamkumarmnp@gmail.com>
2025-11-23 14:59:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from '#components/Navbar'
+import React from 'react'
+// import './App.css'
+
+const App = () => {
+  return (
+   <main>
+    <Navbar/>
+   </main>
+  )
+}
+
+export default App
